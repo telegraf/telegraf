c4d08d3a0d5505d5f49c7f9f194ef582d717a684
Vitaly Domnikov <dotcypress@gmail.com>
2016-07-12 19:30:01 -0700

    add advanced text router

--- examples/keyboard-bot.js
+++ examples/keyboard-bot.js
@@ -8,28 +8,20 @@ bot.on('callback_query', (ctx) => {
   return ctx.answerCallbackQuery(`Oh, ${ctx.callbackQuery.data}! Great choise`, true)
 })
 
-bot.command('/inline', (ctx) => {
-  return ctx.reply('<b>Coke</b> or <i>Pepsi?</i>', Extra.HTML().markup(
-    Markup.inlineKeyboard([
-      { text: 'Coke', callback_data: 'Coke' },
-      { text: 'Pepsi', callback_data: 'Pepsi' }
-    ])))
-})
-
-bot.command('/keyboardonetime', (ctx) => {
+bot.command('/onetime', (ctx) => {
   return ctx.reply('<b>Coke</b> or <i>Pepsi?</i>', Extra
     .HTML()
     .markup((markup) => markup.keyboard(['Coke', 'Pepsi']).resize().oneTime())
   )
 })
 
-bot.command(/\/keyboardwrap (\d+)/, (ctx) => {
+bot.command(/\/wrap (\d+)/, (ctx) => {
   return ctx.reply('Keyboard wrap', Extra.markup(
     Markup.keyboard(['one', 'two', 'three', 'four', 'five', 'six'], {columns: parseInt(ctx.match[1])})
   ))
 })
 
-bot.command('/keyboarpyramid', (ctx) => {
+bot.command('/pyramid', (ctx) => {
   return ctx.reply('Keyboard wrap', Extra.markup(
     Markup.keyboard(['one', 'two', 'three', 'four', 'five', 'six'], {
       wrap: (btn, index, currentRow) => currentRow.length >= (index + 1) / 2
@@ -37,10 +29,18 @@ bot.command('/keyboarpyramid', (ctx) => {
   ))
 })
 
-bot.command('/keyboard', (ctx) => {
+bot.command('/simple', (ctx) => {
   return ctx.replyWithHTML('<b>Coke</b> or <i>Pepsi?</i>', Extra.markup(
     Markup.keyboard(['Coke', 'Pepsi'])
   ))
+})
+
+bot.command('/inline', (ctx) => {
+  return ctx.reply('<b>Coke</b> or <i>Pepsi?</i>', Extra.HTML().markup(
+    Markup.inlineKeyboard([
+      { text: 'Coke', callback_data: 'Coke' },
+      { text: 'Pepsi', callback_data: 'Pepsi' }
+    ])))
 })
 
 bot.startPolling()

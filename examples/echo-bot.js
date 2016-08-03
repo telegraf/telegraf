bc24a3bae5ea4b53c94c20a4a20b733ce3ba8700
Vitaly Domnikov <dotcypress@gmail.com>
2016-08-02 22:34:09 -0700

    update example

--- examples/echo-bot.js
+++ examples/echo-bot.js
@@ -16,4 +16,4 @@ const bot = new Telegraf(process.env.BOT_TOKEN, telegrafOptions)
 const replyOptions = Extra.markup((m) => m.inlineKeyboard([m.urlButton('❤️', 'http://telegraf.js.org')]))
 bot.on('message', (ctx) => ctx.telegram.sendCopy(ctx.from.id, ctx.message, replyOptions))
 
-bot.startPolling()
+bot.startPolling(60)

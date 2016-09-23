67fca3b839fea7392787d2bee97a87bd0dde1832
Vitaly Domnikov <dotcypress@gmail.com>
2016-09-23 15:57:33 -0700

    refactoring

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ Object.assign(module.exports, {
   Telegram: require('./lib/telegram/client'),
   Extra: require('./lib/telegram/extra'),
   Markup: require('./lib/telegram/reply-markup'),
-  platform: require('./lib/telegram/platform'),
   memorySession: require('./lib/memory-session'),
   Composer: require('./lib/composer'),
   Router: require('./lib/router')

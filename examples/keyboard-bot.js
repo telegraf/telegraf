3437764157179eeefe17e3d29e49b2cc6bd5956f
Masoud Ghorbani <uid.mgh@gmail.com>
2016-11-09 18:47:21 +0330

    Update keyboard-bot.js
    fixing lint problems

--- examples/keyboard-bot.js
+++ examples/keyboard-bot.js
@@ -20,19 +20,16 @@ bot.command('onetime', (ctx) => {
 
 bot.command('custom', (ctx) => {
   return ctx.reply('Custom buttons keyboard', Markup
-    .keyboard(
-      [
-          ['🔍 Search', '😎 Popular'], // Row1 with 2 button
-          ['☸ Setting', '📞 Feedback'], // Row2 with 2 button
-          ['📢 Ads', '⭐️ Rate us', '👥 Share'] // Row3 with 3 button
-      ]
-    )
+    .keyboard([
+      ['🔍 Search', '😎 Popular'], // Row1 with 2 button
+      ['☸ Setting', '📞 Feedback'], // Row2 with 2 button
+      ['📢 Ads', '⭐️ Rate us', '👥 Share'] // Row3 with 3 button
+    ])
     .oneTime()
     .resize()
     .extra()
   )
 })
-
 
 bot.command('special', (ctx) => {
   return ctx.reply('Special buttons keyboard', Extra.markup((markup) => {

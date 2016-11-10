e97517b7926c16ed29e3a426a8fd618820a1b8aa
Masoud Ghorbani <uid.mgh@gmail.com>
2016-11-10 12:33:34 +0330

    Create class-router-bot.js
    A simple class(es6) base router

---
+++ examples/class-router-bot.js
@@ -0,0 +1,39 @@
+const Telegraf = require('../')
+const bot = new Telegraf(process.env.BOT_TOKEN)
+
+var BotController = class {
+
+    static MainMenuHandler(ctx) {
+        return ctx.reply('What can i do for you?', Extra.markup( 
+            Markup.keyboard([
+                ['Search', 'Contact'],
+                ['Rate us', 'Ads', 'Share'],
+            ])
+            .resize()
+        ))
+    }
+
+    static StartHandler(ctx) {
+
+        const start_message = `Hey buddy, Welcome to our bot.`
+
+        // Reply with markdown format
+        ctx.reply(start_message, {parse_mode: 'Markdown'}).then(() => {
+        
+            // Calling main menu keyboard
+            this.MainMenuHandler(ctx)
+            
+        })
+    }
+
+    static StopHandler(ctx) {
+        ctx.reply('Stop operation')
+    }
+
+}
+
+bot.command('/main', (ctx) => {BotController.MainMenuHandler(ctx)})
+bot.command('/start', (ctx) => {BotController.StartHandler(ctx)})
+bot.command('/stop', (ctx) => {BotController.StopHandler(ctx)})
+
+bot.startPolling()

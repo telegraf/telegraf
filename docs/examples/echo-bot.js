c0d6952adeff050c86596b7c18ff6174f724d89a
Vitaly Domnikov <dotcypress@gmail.com>
2017-11-12 17:58:09 -0800

    update example

--- docs/examples/echo-bot.js
+++ docs/examples/echo-bot.js
@@ -1,13 +1,14 @@
-const Telegraf = require('../../')
-const Markup = require('../../markup')
+const Telegraf = require('telegraf')
+const Extra = require('telegraf/extra')
+const Markup = require('telegraf/markup')
 
-const replyOptions = Markup.inlineKeyboard([
+const keyboard = Markup.inlineKeyboard([
   Markup.urlButton('❤️', 'http://telegraf.js.org'),
   Markup.callbackButton('Delete', 'delete')
-]).extra()
+])
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 bot.start((ctx) => ctx.reply('Hey there!'))
-bot.on('message', (ctx) => ctx.telegram.sendCopy(ctx.from.id, ctx.message, replyOptions))
+bot.on('message', (ctx) => ctx.telegram.sendCopy(ctx.from.id, ctx.message, Extra.markup(keyboard)))
 bot.action('delete', ({ deleteMessage }) => deleteMessage())
 bot.startPolling()

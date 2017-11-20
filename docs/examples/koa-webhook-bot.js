5c03e1d4d465b41750c197cfa14fa50756b45dd1
Vitaly Domnikov <dotcypress@gmail.com>
2017-11-20 13:44:36 -0800

    update docs

--- docs/examples/koa-webhook-bot.js
+++ docs/examples/koa-webhook-bot.js
@@ -3,10 +3,10 @@ const Koa = require('koa')
 const koaBody = require('koa-body')
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
-bot.on('text', ({ reply }) => reply('Hey there!'))
 // Set telegram webhook
 // npm install -g localtunnel && lt --port 3000
 bot.telegram.setWebhook('https://-----.localtunnel.me/secret-path')
+bot.on('text', ({ reply }) => reply('Hey there!'))
 
 const app = new Koa()
 app.use(koaBody())
@@ -14,5 +14,4 @@ app.use((ctx, next) => ctx.method === 'POST' || ctx.url === '/secret-path'
   ? bot.handleUpdate(ctx.request.body, ctx.response)
   : next()
 )
-
 app.listen(3000)

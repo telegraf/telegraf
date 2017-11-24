4599db7de14410b575f2304e560e40d8a0cc274d
Vitaly Domnikov <dotcypress@gmail.com>
2017-11-24 10:42:35 -0800

    update example

--- docs/examples/wizard-bot.js
+++ docs/examples/wizard-bot.js
@@ -1,20 +1,26 @@
 const Telegraf = require('telegraf')
+const Composer = require('telegraf/composer')
 const session = require('telegraf/session')
 const Stage = require('telegraf/stage')
+const Markup = require('telegraf/markup')
 const WizardScene = require('telegraf/scenes/wizard')
+
+const stepHandler = new Composer()
+stepHandler.action('next', (ctx) => {
+  ctx.reply('Step 2 ')
+  ctx.wizard.next()
+})
+stepHandler.use((ctx) => ctx.replyWithMarkdown('Press `Next` button to contunie'))
 
 const superWizard = new WizardScene('super-wizard',
   (ctx) => {
-    ctx.reply('Step 1')
+    ctx.reply('Step 1', Markup.inlineKeyboard([
+      Markup.urlButton('❤️', 'http://telegraf.js.org'),
+      Markup.callbackButton('➡️ Next', 'next')
+    ]).extra())
     ctx.wizard.next()
   },
-  (ctx) => {
-    if (ctx.message && ctx.message.text !== 'ok') {
-      return ctx.replyWithMarkdown('Send `ok`')
-    }
-    ctx.reply('Step 2 ')
-    ctx.wizard.next()
-  },
+  stepHandler.middleware(),
   (ctx) => {
     ctx.reply('Step 3')
     ctx.wizard.next()

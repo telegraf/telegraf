b6c06c32eeb74cfe7c8a8c04d44ebc73f36feee8
Vitaly Domnikov <dotcypress@gmail.com>
2018-02-02 18:29:57 -0800

    add express example

---
+++ docs/examples/express-webhook-bot.js
@@ -0,0 +1,16 @@
+const Telegraf = require('telegraf')
+const express = require('express')
+
+const bot = new Telegraf(process.env.BOT_TOKEN)
+bot.on('text', ({ replyWithHTML }) => replyWithHTML('<b>Hey there!</b>'))
+
+// Set telegram webhook
+// npm install -g localtunnel && lt --port 3000
+bot.telegram.setWebhook('https://----.localtunnel.me/secret-path')
+
+const app = express()
+app.get('/', (req, res) => res.send('Hello World!'))
+app.use(bot.webhookCallback('/secret-path'))
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!')
+})

85ec35b39a34f1f26a011002f0d1765a59166b45
sekarpdkt <sekarpdkt@gmail.com>
2018-06-30 19:12:18 +0530

    Update webhook-bot.js

--- docs/examples/webhook-bot.js
+++ docs/examples/webhook-bot.js
@@ -1,12 +1,18 @@
 const Telegraf = require('telegraf')
+var localtunnel = require('localtunnel'); //Intall localtunnel first... npm install -g localtunnel 
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 bot.command('image', (ctx) => ctx.replyWithPhoto({ url: 'https://picsum.photos/200/300/?random' }))
 bot.on('text', ({ replyWithHTML }) => replyWithHTML('<b>Hello</b>'))
 
-// Set telegram webhook
-// npm install -g localtunnel && lt --port 3000
-bot.telegram.setWebhook('https://-----.localtunnel.me/secret-path')
+// Start local tunnel and set telegram webhook
+var tunnel =  localtunnel(3000, function(err, tunnel) {
+    if (err) throw err;
+    // the assigned public url for your tunnel
+    // i.e. https://abcdefgjhij.localtunnel.me
+    console.log(tunnel.url)
+    bot.telegram.setWebhook(`${tunnelURL}/telegramTelegrafLocalTunnel`)
+});
 
 // Start https webhook
-bot.startWebhook('/secret-path', null, 3000)
+bot.startWebhook('/telegramTelegrafLocalTunnel', null, 3000)

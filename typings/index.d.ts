0649c34002cab3750b6772ea454d978a87683b26
Álvaro Martínez <alvaro.mart.mart+dev@gmail.com>
2019-04-07 00:31:59 +0200

    fix missing UrlButton compatibility from Markup.inlineKeyboard typings (#645)

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1038,7 +1038,7 @@ export class Markup {
 
   static keyboard(buttons: (Buttons | string)[], options?: object): tt.InlineKeyboardMarkup;
 
-  static inlineKeyboard(buttons: CallbackButton[] | CallbackButton[][], options?: object): tt.InlineKeyboardMarkup;
+  static inlineKeyboard(buttons: CallbackButton[] | CallbackButton[][] | UrlButton[] | UrlButton[][], options?: object): tt.InlineKeyboardMarkup;
 
   static resize(value?: boolean): Markup;
 

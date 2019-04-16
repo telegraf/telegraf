b8fe94ebaac3352d833a16c530779ea2691462fb
Vitaly Domnikov <dotcypress@gmail.com>
2019-04-16 13:15:48 -0700

    update poll example

--- docs/examples/poll-bot.js
+++ docs/examples/poll-bot.js
@@ -3,9 +3,9 @@ const { Composer } = Telegraf
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
-bot.on('poll', (ctx) => ctx.reply('Poll update'))
+bot.on('poll', (ctx) => console.log('Poll update', ctx.poll))
 
-bot.start((ctx) => ctx.telegram.sendPoll('-100500100500100', '2b|!2b', ['True', 'False']))
+bot.start((ctx) => ctx.telegram.sendPoll('group id', '2b|!2b', ['True', 'False']))
 
 bot.command('poll', Composer.groupChat(
   (ctx) => ctx.replyWithPoll('2b|!2b', ['True', 'False'])

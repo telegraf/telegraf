a7d8fd81cb74a834d10549a6e2bf9f70d360e8fb
Aleksey Immuzov <immuzov@gmail.com>
2019-07-25 10:11:07 +0400

    Fix typing

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -567,6 +567,21 @@ export interface Telegram {
   getChatMembersCount(chatId: string | number): Promise<number>
 
   /**
+   * Use this method to restrict a user in a supergroup. The bot must be an administrator in the supergroup for this to work and must have the appropriate admin rights. Pass True for all boolean parameters to lift restrictions from a user. Returns True on success.
+   * @param chatId Unique identifier for the target chat or username of the target supergroup (in the format @supergroupusername)
+   * @param user_id Unique identifier of the target user
+   * @param extra Additional params for restrict chat member
+   * @returns True on success 
+   */
+  restrictChatMember(chatId: string | number, userId: number, extra?: {
+    until_date?: boolean,
+    can_send_messages?: boolean,
+    can_send_media_messages?: boolean,
+    can_send_other_messages?: boolean,
+    can_add_web_page_previews?: boolean
+  }): Promise<boolean>
+
+  /**
    * Use this method for your bot to leave a group, supergroup or channel
    * @param chatId Unique identifier for the target chat or username of the target supergroup or channel (in the format @channelusername)
    * @returns True on success

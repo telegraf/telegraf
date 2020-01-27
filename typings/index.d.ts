050032d73ca05492a3e27388bc72e23d4e273211
Egor Makarenko <egormkn@yandex.ru>
2020-01-28 00:04:06 +0300

    Remove unwanted space

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -279,7 +279,7 @@ export interface ContextMessageUpdate extends Context {
    * @param ok  Specify True if everything is alright (goods are available, etc.) and the bot is ready to proceed with the order. Use False if there are any problems.
    * @param errorMessage Required if ok is False. Error message in human readable form that explains the reason for failure to proceed with the checkout (e.g. "Sorry, somebody just bought the last of our amazing black T-shirts while you were busy filling out your payment details. Please choose a different color or garment!"). Telegram will display this message to the user.
    */
-  answerPreCheckoutQuery (ok: boolean, errorMessage?: string): Promise<boolean>
+  answerPreCheckoutQuery(ok: boolean, errorMessage?: string): Promise<boolean>
 
   /**
    * Use this method to send answers to an inline query.
@@ -436,7 +436,7 @@ export interface Telegram {
    * @param ok  Specify True if everything is alright (goods are available, etc.) and the bot is ready to proceed with the order. Use False if there are any problems.
    * @param errorMessage Required if ok is False. Error message in human readable form that explains the reason for failure to proceed with the checkout (e.g. "Sorry, somebody just bought the last of our amazing black T-shirts while you were busy filling out your payment details. Please choose a different color or garment!"). Telegram will display this message to the user.
    */
-  answerPreCheckoutQuery(preCheckoutQueryId: string, ok: boolean, errorMessage?: string): Promise<boolean>
+  answerPreCheckoutQuery (preCheckoutQueryId: string, ok: boolean, errorMessage?: string): Promise<boolean>
 
   /**
    * Use this method to send answers to an inline query. On success, True is returned.

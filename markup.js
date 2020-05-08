def91c661d77cb4ed8f180aea1ff00cede857c2d
Piterden <efremov.a.denis@gmail.com>
2020-05-08 07:24:55 +0300

    Revert "Apply suggestions from code review"
    This reverts commit d6470f42a83aca14e86ec2f22fd76577b71557ca.

--- markup.js
+++ markup.js
@@ -185,12 +185,12 @@ class Markup {
         endText: '</code>'
       },
       {
-        condition: { type: 'pre' },
+        condition: { type: 'pre', language: true },
         startText: `<pre><code class="language-${entity.language}">`,
         endText: '</code></pre>'
       },
       {
-        condition: { type: 'pre', language: undefined },
+        condition: { type: 'pre', language: false },
         startText: '<pre>',
         endText: '</pre>'
       },
@@ -215,7 +215,9 @@ class Markup {
         endText: '</a>'
       }
     ].find((item) => Object.entries(item.condition)
-      .every(([key, value]) => entity[key] === value))
+      .every(([key, value]) => typeof value === 'boolean'
+        ? Boolean(entity[key]) === value
+        : entity[key] === value))
 
     for (let offset = 0; offset < chars.length; offset++) {
       while (true) {

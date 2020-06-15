ce77f82cd0b37abf43dbd92bfb56d278ddc557ba
岡部 倫太郎 <ejnshtein@gmail.com>
2020-06-15 16:23:12 +0300

    Update typings/context.d.ts
    Co-authored-by: Wojciech Pawlik <woj.pawlik@gmail.com>

--- typings/context.d.ts
+++ typings/context.d.ts
@@ -4,7 +4,7 @@ import * as tt from './telegram-types'
 
 import { Telegram } from './telegram'
 
-export declare class TelegrafContext<StateType = any> {
+export declare class TelegrafContext {
   updateType: tt.UpdateType
   updateSubTypes: tt.MessageSubTypes[]
   update: tt.Update

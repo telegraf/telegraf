60635c3849308c9a59a87847105f54f30359b4d7
Steffen <steffen@indigo-nms.eu>
2020-08-21 13:22:38 +0200

    Make reply_markup Partial in extra.ts

--- src/extra.ts
+++ src/extra.ts
@@ -10,7 +10,7 @@ class Extra {
   reply_to_message_id?: number
   disable_notification?: boolean
   disable_web_page_preview?: boolean
-  reply_markup?: ExtraReplyMessage['reply_markup']
+  reply_markup?: Partial<ExtraReplyMessage['reply_markup']>
   parse_mode?: ParseMode
   static Markup: typeof Markup
   constructor(opts?: ExtraReplyMessage) {
@@ -45,7 +45,7 @@ class Extra {
       markup = markup(new Markup())
     }
     // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
-    this.reply_markup = { ...markup } as ExtraReplyMessage['reply_markup']
+    this.reply_markup = { ...markup }
     return this
   }
 

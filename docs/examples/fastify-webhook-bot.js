9ec3caceb00a85cab8a054785337aa70b601d56f
Erfan Safari <erfanshield@outlook.com>
2020-11-10 22:23:25 +0330

    [docs] use fastify-telegraf (#1168)

--- docs/examples/fastify-webhook-bot.js
+++ docs/examples/fastify-webhook-bot.js
@@ -1,15 +1,25 @@
-const Telegraf = require('telegraf')
-const fastifyApp = require('fastify')()
+const fastify = require('fastify')
+const { Telegraf } = require('telegraf')
+const telegrafPlugin = require('fastify-telegraf')
 
-const bot = new Telegraf(process.env.BOT_TOKEN)
+const { BOT_TOKEN, WEBHOOK_URL } = process.env
+const PORT = process.env.PORT || 3000
+const SECRET_PATH = '/my-secret-path'
+
+if (!WEBHOOK_URL) throw new Error('"WEBHOOK_URL" env var is required!')
+if (!BOT_TOKEN) throw new Error('"BOT_TOKEN" env var is required!')
+
+const bot = new Telegraf(BOT_TOKEN)
+const app = fastify()
+
+app.register(telegrafPlugin, { bot, path: SECRET_PATH })
 
 bot.on('text', ({ reply }) => reply('Hello'))
 
-// Set telegram webhook
-// npm install -g localtunnel && lt --port 3000
-bot.telegram.setWebhook('https://-------.localtunnel.me/secret-path')
+bot.telegram.setWebhook(WEBHOOK_URL).then(() => {
+  console.log('Webhook is set on', WEBHOOK_URL)
+})
 
-fastifyApp.use(bot.webhookCallback('/secret-path'))
-fastifyApp.listen(3000, () => {
-  console.log('Example app listening on port 3000!')
+app.listen(PORT, () => {
+  console.log('Start listening on port', PORT)
 })

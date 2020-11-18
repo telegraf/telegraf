de21c07e3c57f05242f6af66fad16a0f929f50ea
Septs <github@septs.pw>
2020-11-18 21:14:16 +0800

    add aws lambda example code

---
+++ docs/examples/aws-lambda.js
@@ -0,0 +1,12 @@
+const Telegraf = require('telegraf')
+const makeHandler = require('lambda-request-handler')
+
+const bot = new Telegraf(process.env.BOT_TOKEN, {
+  telegram: { webhookReply: true }
+})
+
+bot.start((ctx) => ctx.reply('Hello'))
+
+export const handler = makeHandler(
+  bot.webhookCallback(process.env.BOT_HOOK_PATH ?? '/')
+)

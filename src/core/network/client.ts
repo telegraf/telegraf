845896f6b689ce36a657a541ec3d603f2285b208
Wojciech Pawlik <woj.pawlik@gmail.com>
2020-12-10 22:40:58 +0100

    Add 2 minute timeout to every request

--- src/core/network/client.ts
+++ src/core/network/client.ts
@@ -363,6 +363,7 @@ class ApiClient {
     const apiUrl = `${options.apiRoot}/bot${token}/${method}`
     config.agent = options.agent
     config.signal = signal
+    config.timeout = 120000 // 2 minutes
     const res = await fetch(apiUrl, config)
     const data = await res.json()
     if (!data.ok) {

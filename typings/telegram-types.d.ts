4c53fcc37ca7a9d2cecce3ee94d9ca03d05a6258
Josh Dies <LetItCode@users.noreply.github.com>
2021-01-14 15:18:30 +0300

    correct params in sendLocation

--- typings/telegram-types.d.ts
+++ typings/telegram-types.d.ts
@@ -408,9 +408,29 @@ export interface ExtraInvoice extends ExtraReplyMessage {
 
 export interface ExtraLocation extends ExtraReplyMessage {
   /**
+   * The radius of uncertainty for the location, measured in meters; 0-1500
+   */
+  horizontal_accuracy?: number
+
+  /**
    * Period in seconds for which the location will be updated (should be between 60 and 86400)
    */
   live_period?: number
+
+  /**
+   * For live locations, a direction in which the user is moving, in degrees. Must be between 1 and 360 if specified.
+   */
+  heading?: number
+  
+  /**
+   * For live locations, a maximum distance for proximity alerts about approaching another chat member, in meters. Must be between 1 and 100000 if specified.
+   */
+  proximity_alert_radius?: number
+  
+  /**
+   * Pass True, if the message should be sent even if the specified replied-to message is not found
+   */
+  allow_sending_without_reply?: boolean
 
   /**
    * Does not exist, see https://core.telegram.org/bots/api#sendlocation

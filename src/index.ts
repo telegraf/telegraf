a57dbcdd4e706681cdb390a086e1cf90a4cfee4d
Wojciech Pawlik <woj.pawlik@gmail.com>
2021-01-14 15:04:55 +0100

    Export `TelegramError`; fixes #1289

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ export { Context } from './context'
 export { Composer } from './composer'
 export { Middleware, MiddlewareFn } from './middleware'
 export { Router } from './router'
+export { TelegramError } from './core/network/error'
 
 export * as Markup from './markup'
 

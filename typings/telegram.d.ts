edf35913d6582144db305998ee8e30aff97117dc
Loskir <kl@sinker.ru>
2021-01-14 15:46:13 +0300

    fix JSDoc for promoteChatMember

--- typings/telegram.d.ts
+++ typings/telegram.d.ts
@@ -693,6 +693,7 @@ export declare class Telegram extends ApiClient {
    * Use this method to promote or demote a user in a supergroup or a channel. The bot must be an administrator in the chat for this to work and must have the appropriate admin rights. Pass False for all boolean parameters to demote a user.
    * @param chatId Unique identifier for the target chat or username of the target channel (in the format `@channelusername`)
    * @param userId Unique identifier of the target user
+   * @param extra Extra parameters for promoteChatMember
    * @returns True on success
    */
   promoteChatMember(

d2e3b6f37654aeb44df16df52540f8e93f4a658b
Loskir <kl@sinker.ru>
2021-02-06 20:31:49 +0300

    Add tests for scenes and wizards

--- test/telegraf.js
+++ test/telegraf.js
@@ -1,5 +1,5 @@
 const test = require('ava')
-const { Telegraf, session } = require('../')
+const { Telegraf, session, Scenes } = require('../')
 
 function createBot (...args) {
   const bot = new Telegraf(...args)
@@ -155,4 +155,61 @@ test('should respect webhookReply runtime change (per request)', async (t) => {
   await t.throwsAsync(bot.handleUpdate({ message: BaseTextMessage }, res))
   t.true(res.writableEnded)
   t.is(res.body, undefined)
+})
+
+test('should execute enter middleware in scene', (t) => {
+  const bot = createBot()
+  const scene = new Scenes.BaseScene('hello')
+  scene.enter((ctx) => t.pass())
+  const stage = new Scenes.Stage([scene])
+  stage.use((ctx) => ctx.scene.enter('hello'))
+  bot.use(session({ getSessionKey: (ctx) => '1' }))
+  bot.use(stage)
+  return bot.handleUpdate({ message: BaseTextMessage })
+})
+
+test('should execute enter middleware in wizard scene', (t) => {
+  const bot = createBot()
+  const scene = new Scenes.WizardScene('hello', [])
+  scene.enter((ctx) => t.pass())
+  const stage = new Scenes.Stage([scene])
+  stage.use((ctx) => ctx.scene.enter('hello'))
+  bot.use(session({ getSessionKey: (ctx) => '1' }))
+  bot.use(stage)
+  return bot.handleUpdate({ message: BaseTextMessage })
+})
+
+test('should execute first step in wizard scene on enter', (t) => {
+  const bot = createBot()
+  const scene = new Scenes.WizardScene(
+    'hello',
+    (ctx) => {
+      t.pass()
+    }
+  )
+  const stage = new Scenes.Stage([scene])
+  stage.use((ctx) => ctx.scene.enter('hello'))
+  bot.use(session({ getSessionKey: (ctx) => '1' }))
+  bot.use(stage)
+  return bot.handleUpdate({ message: BaseTextMessage })
+})
+
+test('should execute both enter middleware and first step in wizard scene on enter', (t) => {
+  t.plan(2)
+  const bot = createBot()
+  const scene = new Scenes.WizardScene(
+    'hello',
+    (ctx) => {
+      t.pass()
+    }
+  )
+  scene.enter((ctx, next) => {
+    t.pass()
+    return next()
+  })
+  const stage = new Scenes.Stage([scene])
+  stage.use((ctx) => ctx.scene.enter('hello'))
+  bot.use(session({ getSessionKey: (ctx) => '1' }))
+  bot.use(stage)
+  return bot.handleUpdate({ message: BaseTextMessage })
 })

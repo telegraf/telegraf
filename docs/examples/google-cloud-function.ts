d1d3eb5dfb83380460ce98fe72e4d09479fbce7d
Wojciech Pawlik <woj.pawlik@gmail.com>
2021-02-16 21:45:21 +0100

    Misc

--- docs/examples/google-cloud-function.ts
+++ docs/examples/google-cloud-function.ts
@@ -1,27 +1,24 @@
 import { Telegraf } from 'telegraf'
 
-const BOT_TOKEN = process.env.BOT_TOKEN ?? '<BOT TOKEN HERE>'
-const PROJECT_ID = process.env.PROJECT_ID ?? '<Google Project ID>'
-const FUNCTION_NAME = process.env.FUNCTION_NAME ?? 'botFunction'
-const REGION = process.env.REGION ?? 'us-central1'
+const { BOT_TOKEN, FUNCTION_NAME, PROJECT_ID, REGION } = process.env
+
+if (BOT_TOKEN === undefined) {
+  throw new TypeError('BOT_TOKEN must be provided!')
+}
 
 const bot = new Telegraf(BOT_TOKEN)
 
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
 bot.telegram.setWebhook(
-  `https://${REGION}-${PROJECT_ID}.cloudfunctions.net/${FUNCTION_NAME}`
+  `https://${REGION!}-${PROJECT_ID!}.cloudfunctions.net/${FUNCTION_NAME!}`
 )
 
 bot.command('hello', (ctx) => ctx.reply('Hello, friend!'))
 
 export const botFunction = async (req: any, res: any) => {
-  console.log(req.body)
   try {
     await bot.handleUpdate(req.body)
-    console.log('Success')
-  } catch (err) {
-    console.log(err)
   } finally {
-    res.status(200).send('Success')
+    res.status(200).end()
   }
 }

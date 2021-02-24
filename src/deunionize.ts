5fa5b5865a3d5169a7d3fbb738dfc4cbb92cdc31
Wojciech Pawlik <woj.pawlik@gmail.com>
2021-02-24 21:50:58 +0100

    Rewrite `Deunionize`

--- src/deunionize.ts
+++ src/deunionize.ts
@@ -6,12 +6,16 @@ export type PropOr<
 
 export type UnionKeys<T> = T extends unknown ? keyof T : never
 
-export type Deunionize<T extends object | undefined> =
-  | ([undefined] extends [T] ? undefined : never)
-  | {
-      [K in UnionKeys<T>]: PropOr<NonNullable<T>, K, undefined>
-    }
+type AddOptionalKeys<K extends PropertyKey> = { [P in K]?: undefined }
 
+export type Deunionize<
+  B extends object | undefined,
+  T extends B = B
+> = T extends object ? T & AddOptionalKeys<Exclude<UnionKeys<B>, keyof T>> : T
+
+/**
+ * Expose properties from all union variants
+ */
 export function deunionize<T extends object | undefined>(t: T) {
   return t as Deunionize<T>
 }

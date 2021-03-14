505844a8c3fcc58d8b6260dcbc88591d55519fc1
Wojciech Pawlik <woj.pawlik@gmail.com>
2021-03-14 12:05:19 +0100

    Document `deunionize`

--- src/deunionize.ts
+++ src/deunionize.ts
@@ -6,15 +6,20 @@ export type PropOr<
 
 export type UnionKeys<T> = T extends unknown ? keyof T : never
 
-type AddOptionalKeys<K extends PropertyKey> = { [P in K]?: undefined }
+type AddOptionalKeys<K extends PropertyKey> = { readonly [P in K]?: undefined }
 
+/**
+ * @see https://millsp.github.io/ts-toolbelt/modules/union_strict.html
+ */
 export type Deunionize<
   B extends object | undefined,
   T extends B = B
 > = T extends object ? T & AddOptionalKeys<Exclude<UnionKeys<B>, keyof T>> : T
 
 /**
- * Expose properties from all union variants
+ * Expose properties from all union variants.
+ * @see https://github.com/telegraf/telegraf/issues/1388#issuecomment-791573609
+ * @see https://millsp.github.io/ts-toolbelt/modules/union_strict.html
  */
 export function deunionize<T extends object | undefined>(t: T) {
   return t as Deunionize<T>

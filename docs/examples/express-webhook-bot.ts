7287b8f44dab1a57690e41df2d4a669188a29288
Abhijith N T <abhijithnt64@gmail.com>
2021-06-21 14:41:26 +0530

    #1456

--- docs/examples/express-webhook-bot.ts
+++ docs/examples/express-webhook-bot.ts
@@ -1,30 +1,28 @@
 /* eslint-disable @typescript-eslint/no-floating-promises */
 
 // @ts-expect-error `npm install express && npm install --save-dev @types/express`
-import express, { Request, Response } from 'express'
-import { Telegraf } from 'telegraf'
+import express = require('express');
+import { Request, Response } from "express";
+import { Telegraf } from "telegraf";
 
-const token = process.env.BOT_TOKEN
+const token = process.env.BOT_TOKEN;
 if (token === undefined) {
-  throw new Error('BOT_TOKEN must be provided!')
+  throw new Error("BOT_TOKEN must be provided!");
 }
 
-const bot = new Telegraf(token)
-// Set the bot response
-bot.on('text', (ctx) => ctx.replyWithHTML('<b>Hello</b>'))
+const bot = new Telegraf(token);
 
-const secretPath = `/telegraf/${bot.secretPathComponent()}`
+bot.on("text", (ctx) => ctx.replyWithHTML("<b>Hello</b>"));
 
-// Set telegram webhook
-// npm install -g localtunnel && lt --port 3000
-bot.telegram.setWebhook(`https://----.localtunnel.me${secretPath}`)
+const app = express();
+app.get("/", (req: Request, res: Response) => res.send("Hello World!"));
 
-const app = express()
-app.get('/', (req: Request, res: Response) => res.send('Hello World!'))
-// Set the bot API endpoint
-app.use(bot.webhookCallback(secretPath))
+
+app.use(bot.webhookCallback('/secret-path'))
+bot.telegram.setWebhook('https://server.tld:8443/secret-path')
+
 app.listen(3000, () => {
-  console.log('Example app listening on port 3000!')
-})
+  console.log("Example app listening on port 3000!");
+});
 
 // No need to call bot.launch()

af48f7e458162bca22ee038e331069813094cf5f
awx <callmeoniichandesu@gmail.com>
2021-06-23 10:01:41 +0700

    Fixing webhook middleware issue Telegraf#1476

--- src/core/network/webhook.ts
+++ src/core/network/webhook.ts
@@ -4,12 +4,15 @@ import { Update } from '../types/typegram'
 import safeCompare = require('safe-compare')
 const debug = d('telegraf:webhook')
 
+interface Body {
+  body?: Update
+}
 export default function (
   hookPath: string,
   updateHandler: (update: Update, res: http.ServerResponse) => Promise<void>
 ) {
   return async (
-    req: http.IncomingMessage,
+    req: http.IncomingMessage & Body,
     res: http.ServerResponse,
     next = (): void => {
       res.statusCode = 403
@@ -21,11 +24,19 @@ export default function (
     if (req.method !== 'POST' || !safeCompare(hookPath, req.url!)) {
       return next()
     }
+
+    let update: Update
+
+    if (req.body != null) {
+      update = req.body
+      await updateHandler(update, res)
+      return
+    }
+
     let body = ''
     for await (const chunk of req) {
       body += String(chunk)
     }
-    let update: Update
     try {
       update = JSON.parse(body)
     } catch (error: unknown) {

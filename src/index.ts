546aebd1349bd701753154707119019a279b2c38
Muthu Kumar <muthukumar@thefeathers.in>
2022-09-28 16:33:55 +0530

    chore: export errors as namespace

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ export { Telegraf } from './telegraf'
 export { Context } from './context'
 export { Composer, NarrowedContext } from './composer'
 export { Middleware, MiddlewareFn, MiddlewareObj } from './middleware'
-export { TelegramError } from './core/network/error'
+export * as errors from './core/network/error'
 export { Telegram } from './telegram'
 export * as Types from './telegram-types'
 

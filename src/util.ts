31392306b80a0b596c9b714814d671dc9f706bee
Muthu Kumar <muthukumar@thefeathers.in>
2023-01-30 03:48:09 +0530

    feat(fmt): join() helper, fixes, and tests

--- src/util.ts
+++ src/util.ts
@@ -40,6 +40,25 @@ export type Guarded<F> =
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   F extends (x: any) => x is infer T ? T : never
 
+export function* zip<X, Y>(xs: Iterable<X>, ys: Iterable<Y>): Iterable<X | Y> {
+  const x = xs[Symbol.iterator]()
+  const y = ys[Symbol.iterator]()
+  let x1 = x.next()
+  let y1 = y.next()
+
+  while (!x1.done) {
+    yield x1.value
+    if (!y1.done) yield y1.value
+    x1 = x.next()
+    y1 = y.next()
+  }
+
+  while (!y1.done) {
+    yield y1.value
+    y1 = y.next()
+  }
+}
+
 export function deprecate(
   method: string,
   ignorable: string | undefined,

6e18f66569cc04f172e1cd68d8f7ebd6e7b00f81
Muthu Kumar <muthukumar@thefeathers.in>
2023-04-05 03:07:21 +0530

    feat: add FmtString support to replace

--- src/core/helpers/formatting.ts
+++ src/core/helpers/formatting.ts
@@ -81,46 +81,129 @@ export const join = (
   return result
 }
 
+interface EntityCompare {
+  offset: number
+  length: number
+}
+
+/** get the starting of the entity */
+const starts = (e: EntityCompare) => e.offset
+
+/** get the ending of the entity */
+const ends = (e: EntityCompare) => e.offset + e.length
+
+const before = (A: EntityCompare, B: EntityCompare) =>
+  // B ends before A starts
+  ends(B) <= starts(A)
+
+const after = (A: EntityCompare, B: EntityCompare) =>
+  // B starts after A ends
+  starts(B) >= ends(A)
+
+const inside = (A: EntityCompare, B: EntityCompare) =>
+  // B starts with/after A and ends before A
+  (starts(B) >= starts(A) && ends(B) < ends(A)) ||
+  // B starts after A and ends before/with A
+  (starts(B) > starts(A) && ends(B) <= ends(A))
+
+const contains = (A: EntityCompare, B: EntityCompare) =>
+  // B starts before/with A and ends with/after A
+  starts(B) <= starts(A) && ends(B) >= ends(A)
+
+const endsInside = (A: EntityCompare, B: EntityCompare) =>
+  // B starts before A starts, ends after A starts, ends before B ends
+  starts(B) < starts(A) && ends(B) > starts(A) && ends(B) < ends(A)
+
+const startsInside = (A: EntityCompare, B: EntityCompare) =>
+  // B starts after A, starts before A ends, ends after A
+  starts(B) > starts(A) && starts(B) < ends(A) && ends(B) > ends(A)
+
 export const replace = (
-  source: string,
+  source: string | FmtString,
   search: string | RegExp,
-  value: string | FmtString
+  value: string | FmtString | ((match: string) => string | FmtString)
 ): FmtString => {
-  const v = FmtString.normalise(value)
-  let entities: MessageEntity[] | undefined = undefined
+  source = FmtString.normalise(source)
+
+  let text = source.text
+  let entities: MessageEntity[] | undefined = source.entities
+
+  function fixEntities(offset: number, length: number, correction: number) {
+    const A = { offset, length }
+
+    return (entities || [])
+      .map((E) => {
+        if (before(A, E)) return E
+        if (inside(A, E)) return
+        if (after(A, E)) return { ...E, offset: E.offset + correction }
+        if (contains(A, E)) return { ...E, length: E.length + correction }
+        if (endsInside(A, E))
+          return { ...E, length: E.length - (ends(E) - starts(A)) }
+        if (startsInside(A, E)) {
+          const entityInside = ends(A) - starts(E)
+          return {
+            ...E,
+            offset: E.offset + entityInside + correction,
+            length: E.length - entityInside,
+          }
+        }
+
+        throw new Error(
+          'Entity found in an unexpected condition. ' +
+            'This is probably a bug in telegraf. ' +
+            'You should report this to https://github.com/telegraf/telegraf/issues'
+        )
+      })
+      .filter((x): x is MessageEntity => Boolean(x))
+  }
 
   if (typeof search === 'string') {
-    const offset = source.indexOf(search)
+    const replace = FmtString.normalise(
+      typeof value === 'function' ? value(search) : value
+    )
+    const offset = text.indexOf(search)
     const length = search.length
-    source = source.slice(0, offset) + v.text + source.slice(offset + length)
-    entities = v.entities?.map((e) => ({ ...e, offset: e.offset + offset }))
+    text = text.slice(0, offset) + replace.text + text.slice(offset + length)
+    const currentCorrection = replace.text.length - length
+    entities = [
+      ...fixEntities(offset, length, currentCorrection),
+      ...(replace.entities || []).map((E) => ({
+        ...E,
+        offset: E.offset + offset,
+      })),
+    ]
   } else {
-    let index = 0 // context position in source string
+    let index = 0 // context position in text string
     let acc = '' // incremental return value
     let correction = 0
 
     let regexArray
-    while ((regexArray = search.exec(source))) {
+    while ((regexArray = search.exec(text))) {
+      const match = regexArray[0]
       const offset = regexArray.index
-      const length = regexArray[0].length
-      acc += source.slice(index, offset) + v.text
+      const length = match.length
+      const replace = FmtString.normalise(
+        typeof value === 'function' ? value(match) : value
+      )
+      acc += text.slice(index, offset) + replace.text
+      const currentCorrection = replace.text.length - length
 
-      if (v.entities && v.entities.length)
-        (entities ??= []).push(
-          ...v.entities.map((e) => ({
-            ...e,
-            offset: e.offset + offset + correction,
-          }))
-        )
+      entities = [
+        ...fixEntities(offset + correction, length, currentCorrection),
+        ...(replace.entities || []).map((E) => ({
+          ...E,
+          offset: E.offset + offset + correction,
+        })),
+      ]
 
+      correction += currentCorrection
       index = offset + length
-      correction += v.text.length - length
     }
 
-    source = acc + source.slice(index)
+    text = acc + text.slice(index)
   }
 
-  return new FmtString(source, entities)
+  return new FmtString(text, entities)
 }
 
 /** Internal constructor for all fmt helpers */

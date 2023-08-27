f0978fb74ef223c98dbfdb333dbfee8337d2ea74
Muthu Kumar <muthukumar@thefeathers.in>
2023-08-27 06:08:41 +0530

    test(session): add custom property test
    Simplify older tests

--- test/session.js
+++ test/session.js
@@ -54,19 +54,17 @@ test('must resist session racing (with sync store)', (t) =>
       // pretend there are other middlewares that slow down by a random amount
       bot.use(genericAsyncMiddleware)
 
-      Promise.all(
+      return Promise.all(
         Array
           // create 100 text updates and fire them all at once
           .from({ length: 100 }, Fixtures.message.text)
           .map((fixture) => bot.handleUpdate(fixture))
-      ).then(() => {
-        const entries = [...map.entries()]
-        t.deepEqual(entries.length, 1)
-        const [key, value] = entries[0]
-        t.deepEqual(key, '1:1')
-        t.deepEqual(value, { count: 100 })
-        resolve(true)
-      })
+      )
+        .then(() => {
+          t.deepEqual(map.size, 1)
+          t.deepEqual(map.get('1:1'), { count: 100 })
+        })
+        .then(() => resolve(true))
     })
   ))
 
@@ -107,29 +105,21 @@ test('must resist session racing (with async store)', (t) =>
       // pretend there are other middlewares that slow down by a random amount
       bot.use(genericAsyncMiddleware)
 
-      Promise.all(
+      return Promise.all(
         Array
           // create 100 text updates and fire them all at once
           .from({ length: 100 }, Fixtures.message.text)
           .map((fixture) => bot.handleUpdate(fixture))
-      ).then(() => {
-        const entries = [...map.entries()]
-        t.deepEqual(entries.length, 1)
-        const [key, value] = entries[0]
-        t.deepEqual(key, '1:1')
-        t.deepEqual(value, { count: 100 })
-        resolve(true)
-      })
+      )
+        .then(() => {
+          t.deepEqual(map.size, 1)
+          t.deepEqual(map.get('1:1'), { count: 100 })
+        })
+        .then(() => resolve(true))
     })
   ))
 
-/*
-
-This test is the same as above, but a defaultSession is passed.
-It tests that defaultSession is not erraneously stored in the db when it's not used.
-
-*/
-test('must not write default session back if session not touched', (t) =>
+test('must not write session back if session not touched after defaultSession was passed', (t) =>
   t.notThrowsAsync(
     new Promise((resolve) => {
       /** @type {import('..').Telegraf<MyCtx>} */
@@ -145,26 +135,18 @@ test('must not write default session back if session not touched', (t) =>
         // ctx.session is not touched
       })
 
-      Promise.all(
+      return Promise.all(
         Array
           // create 100 text message updates and fire them all at once
           .from({ length: 100 }, Fixtures.message.text)
           .map((fixture) => bot.handleUpdate(fixture))
-      ).then(() => {
-        const entries = [...map.entries()]
-        t.deepEqual(entries.length, 0)
-        resolve(true)
-      })
+      )
+        .then(() => t.deepEqual(map.size, 0))
+        .then(() => resolve(true))
     })
   ))
 
-/*
-
-This test is the same as above, but a defaultSession is passed.
-It tests that defaultSession is not erraneously stored in the db when it's not used.
-
-*/
-test('must write default session back if session was touched', (t) =>
+test('must write session back if session was touched after defaultSession is passed', (t) =>
   t.notThrowsAsync(
     new Promise((resolve) => {
       /** @type {import('..').Telegraf<MyCtx>} */
@@ -179,7 +161,7 @@ test('must write default session back if session was touched', (t) =>
         await randSleep(200)
       })
 
-      Promise.all(
+      return Promise.all(
         Array
           // create 100 text message updates and fire them all at once
           .from({ length: 100 }, Fixtures.message.text)
@@ -192,5 +174,62 @@ test('must write default session back if session was touched', (t) =>
         t.deepEqual(value, { count: 100 })
         resolve(true)
       })
+    })
+  ))
+
+test('multiple sessions can be used independently without conflict', (t) =>
+  t.notThrowsAsync(
+    new Promise((resolve) => {
+      /** @type {import('..').Telegraf<MyCtx & { chatSession: { chatCount: number } }>} */
+      const bot = createBot()
+
+      const { store, map } = SyncStore()
+
+      // first session, ctx.session
+      bot.use(
+        session({
+          store,
+          defaultSession: () => ({ count: 0 }),
+        })
+      )
+
+      const { store: chatStore, map: chatStoreMap } = SyncStore()
+
+      // second session, ctx.chatSession
+      bot.use(
+        session({
+          property: 'chatSession',
+          getSessionKey: (ctx) => ctx.chat && String(ctx.chat.id),
+          store: chatStore,
+          defaultSession: () => ({ chatCount: 0 }),
+        })
+      )
+
+      bot.on('message', async (ctx) => {
+        ctx.session.count++
+        ctx.chatSession.chatCount++
+        // pretend we make an API call, etc
+        await randSleep(200)
+      })
+
+      return Promise.all(
+        Array
+          // create 100 text message updates and fire them all at once
+          .from({ length: 100 }, Fixtures.message.text)
+          // get different chatIds
+          .map((fixture, id) => ((fixture.message.chat.id = id), fixture))
+          .map((fixture) => bot.handleUpdate(fixture))
+      )
+        .then(() => {
+          t.deepEqual(map.size, 100)
+          for (let chatId = 0; chatId < 100; chatId++)
+            t.deepEqual(map.get(`1:${chatId}`), { count: 1 })
+        })
+        .then(() => {
+          t.deepEqual(chatStoreMap.size, 100)
+          for (let chatId = 0; chatId < 100; chatId++)
+            t.deepEqual(chatStoreMap.get(String(chatId)), { chatCount: 1 })
+        })
+        .then(() => resolve(true))
     })
   ))

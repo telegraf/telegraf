b85797ca010b2b20955be9934a207cb167f24cfb
Muthu Kumar <muthukumar@thefeathers.in>
2023-09-22 21:39:56 +0530

    chore: add todo

--- src/core/helpers/compact.ts
+++ src/core/helpers/compact.ts
@@ -6,13 +6,13 @@ export function compactOptions<T extends { [key: string]: unknown }>(
   }
 
   const compacted: Partial<T> = {}
-  for (const key in options) {
+  for (const key in options)
     if (
+      // todo(mkr): replace with Object.hasOwn in v5 (Node 16+)
       Object.prototype.hasOwnProperty.call(options, key) &&
       options[key] !== undefined
-    ) {
+    )
       compacted[key] = options[key]
-    }
-  }
+
   return compacted as T | undefined
 }

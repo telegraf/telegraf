af6ac9602434b2925ed24a15994e3326170f431a
Muthu Kumar <muthukumar@thefeathers.in>
2023-12-31 02:28:40 +0530

    feat: ultra strict fmt helpers

--- src/format.ts
+++ src/format.ts
@@ -1,21 +1,72 @@
 import { User } from '@telegraf/types'
-import { FmtString, _fmt, linkOrMention, join } from './core/helpers/formatting'
+import {
+  FmtString,
+  createFmt,
+  linkOrMention,
+  join,
+} from './core/helpers/formatting'
 
 export { FmtString }
 
-const fmt = _fmt()
-const bold = _fmt('bold')
-const italic = _fmt('italic')
-const spoiler = _fmt('spoiler')
-const strikethrough = _fmt('strikethrough')
-const underline = _fmt('underline')
-const code = _fmt('code')
-const pre = (language: string) => _fmt('pre', { language })
+type Nestable<Kind extends string> = string | number | boolean | FmtString<Kind>
+type Nesting<Kind extends string> = [
+  parts: Nestable<Kind> | readonly Nestable<Kind>[],
+  ...items: Nestable<Kind>[],
+]
+type Nests<Is extends string, Kind extends string> = (
+  ...args: Nesting<Kind>
+) => FmtString<Is>
 
-const link = (content: string | FmtString, url: string) =>
-  linkOrMention(content, { type: 'text_link', url })
+const fmt = createFmt()
 
-const mention = (name: string | FmtString, user: number | User) =>
+const bold = createFmt('bold') as Nests<
+  'bold',
+  'bold' | 'italic' | 'underline' | 'strikethrough' | 'spoiler'
+>
+
+const italic = createFmt('italic') as Nests<
+  'italic',
+  'bold' | 'italic' | 'underline' | 'strikethrough' | 'spoiler'
+>
+
+const spoiler = createFmt('spoiler') as Nests<
+  'spoiler',
+  'bold' | 'italic' | 'underline' | 'strikethrough' | 'spoiler'
+>
+
+const strikethrough = createFmt('strikethrough') as Nests<
+  'strikethrough',
+  'bold' | 'italic' | 'underline' | 'strikethrough' | 'spoiler'
+>
+
+const underline = createFmt('underline') as Nests<
+  'underline',
+  'bold' | 'italic' | 'underline' | 'strikethrough' | 'spoiler'
+>
+
+const code = createFmt('code') as Nests<'code', never>
+
+const quote = createFmt('blockquote') as Nests<
+  'blockquote',
+  'bold' | 'italic' | 'underline' | 'strikethrough' | 'spoiler' | 'code'
+>
+
+const pre = (language: string) =>
+  createFmt('pre', { language }) as Nests<'pre', never>
+
+const link = (
+  content: Nestable<
+    'bold' | 'italic' | 'underline' | 'strikethrough' | 'spoiler' | 'code'
+  >,
+  url: string
+) => linkOrMention(content, { type: 'text_link', url })
+
+const mention = (
+  name: Nestable<
+    'bold' | 'italic' | 'underline' | 'strikethrough' | 'spoiler' | 'code'
+  >,
+  user: number | User
+) =>
   typeof user === 'number'
     ? link(name, 'tg://user?id=' + user)
     : linkOrMention(name, { type: 'text_mention', user })
@@ -28,6 +79,7 @@ export {
   strikethrough,
   underline,
   code,
+  quote,
   pre,
   link,
   mention,

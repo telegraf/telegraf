8639de514faca19d4ce899b69281ad0026b39304
Muthu Kumar <muthukumar@thefeathers.in>
2024-02-19 15:42:31 +0530

    build: expose.ts script to expose arbitrary modules from package script

---
+++ build/expose.ts
@@ -0,0 +1,30 @@
+import { readFileSync, writeFileSync } from 'fs'
+
+const modules = process.argv.slice(2)
+
+function write(module: string, script: string, types: string) {
+  writeFileSync(`./${module}.js`, script)
+  writeFileSync(`./${module}.d.ts`, types)
+}
+
+const pkg = JSON.parse(readFileSync('package.json', 'utf8'))
+
+for (const module of modules) {
+  write(
+    module,
+    `module.exports = require('./lib/${module}')\n`,
+    `export * from './typings/${module}'\n`
+  )
+
+  pkg.exports[`./${module}`] = {
+    types: `./${module}.d.ts`,
+    default: `./${module}.js`,
+  }
+}
+
+writeFileSync('package.json', JSON.stringify(pkg, null, 2))
+
+writeFileSync(
+  '.eslintignore',
+  modules.map((module) => `/${module}.*`).join('\n')
+)
--- types.d.ts
+++
@@ -1,3 +0,0 @@
-/** Experimental exports, shape of type exports may change in the future */
-export * from './typings/core/types/typegram'
-export * as Convenience from './typings/telegram-types'

b68b9611a29795b22dd9c27674dd61e7c61bf602
Muthu Kumar <muthukumar@thefeathers.in>
2024-02-28 23:34:42 +0530

    tempfix: except link_preview_options in includesMedia: v5 doesn't have this check, so this fix dies in v4

--- src/core/network/client.ts
+++ src/core/network/client.ts
@@ -77,7 +77,9 @@ const DEFAULT_OPTIONS: ApiClient.Options = {
 }
 
 function includesMedia(payload: Record<string, unknown>) {
-  return Object.values(payload).some((value) => {
+  return Object.entries(payload).some(([key, value]) => {
+    if (key !== 'link_preview_options') return false
+
     if (Array.isArray(value)) {
       return value.some(
         ({ media }) =>
